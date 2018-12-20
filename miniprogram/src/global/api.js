2d62fc9c4addc56e451fd91c7a43c0838887ccb6
Lea <452224689@qq.com>
2018-12-20 14:56:49 +0800

    update#api

--- miniprogram/src/global/api.js
+++ miniprogram/src/global/api.js
@@ -1,5 +1,5 @@
-// const ApiOrigin = 'http://leo.primercss.cn';
-const ApiOrigin = 'http://api.rtc.test';
+const ApiOrigin = 'http://leo.primercss.cn';
+// 'http://api.rtc.test'
 const RTCApi = 'https://official.opensso.tencent-cloud.com';
 
 const WEB_CONFIG = {
